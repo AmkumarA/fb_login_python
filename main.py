e9d111158f297dcbbb57a2d5eb03ef77c6973141
AmkumarA <amit123.ap62@gmail.com>
2025-05-15 18:04:44 +0530

    full code

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, redirect, session,url_for,render_template,request
+from dotenv import load_dotenv
+import os
+import requests
+
+app = Flask(__name__)
+app.secret_key='mynameamkumarA'
+
+# facebook data ----
+FB_APP_SECRET=os.getenv('FB_APP_SECRET')
+FB_APP_ID=os.getenv('FB_APP_ID')
+redirectUri = 'http://localhost:5000/facebook/callback'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/facebook/login')
+def facebook_login():
+        print("5444") 
+        redirectUri = "http://localhost:5000/facebook/callback"
+        url = redirect(f"https://www.facebook.com/v22.0/dialog/oauth?client_id={FB_APP_ID}&redirect_uri={redirectUri}&scope=email")
+        print("url", url)
+        return url
+
+@app.route('/facebook/callback')
+def facebook_callback():
+      print("544444444444444444444444444444444444444444444444")
+      code= request.args.get('code')
+      print("aaa",code)
+      if code:
+           user_response = requests.get(
+                  f"https://www.facebook.com/v22.0/oauth/access_token?client_id={FB_APP_ID}&redirect_uri={redirectUri}&client_scret={FB_APP_SECRET}&code={code}"
+            )
+           print("456465",user_response)
+           data = user_response.json()
+
+           if 'access_token' in data:
+                 session['access_token']=data['access_token']
+
+                 user_response= requests.get(
+                       f"https://graph.facebook.com/v22.0/me?fields=id,name,email&access_token={data['access_token']}"
+                 )
+                 print("user response",user_response)
+                 return redirect('/')
+if __name__ == '__main__':
+      app.run(debug=True)  

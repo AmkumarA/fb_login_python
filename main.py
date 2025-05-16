7c65fe8b2eda193430d255890553977320e872f0
AmkumarA <amit123.ap62@gmail.com>
2025-05-16 10:08:22 +0530

    added port on main file

--- main.py
+++ main.py
@@ -43,5 +43,6 @@ def facebook_callback():
                  )
                  print("user response",user_response)
                  return redirect('/')
-if __name__ == '__main__':
-      app.run(debug=True)  
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port,debug=True)  
